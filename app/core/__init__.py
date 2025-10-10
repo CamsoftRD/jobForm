c6ce2d291ced02ab140af143541e5342113ed286
esmerlinp <esmerlinp@gmail.com>
2025-10-10 10:31:58 -0400

    actualización de las URLs de la API para el entorno de QA en secrets.toml y ajustes en la función fetch_data

--- app/core/__init__.py
+++ app/core/__init__.py
@@ -23,19 +23,19 @@ def fetch_data(endpoint, method="GET", params=None, body_params=None, headers=No
             "Content-Type": "application/json",
             "x-ui-culture": "es-DO",
             "x-api-key": "002002032323232320002SSS",
-            "x-ui-domain": "mallen.triple.com.do"
+            "x-ui-domain": "mallenqa.triple.com.do"
         }
         api = base_url
         
         if modulo ==  "reclutamiento":
-             api = "https://api.reclutamiento.triple.com.do"
+             api = "https://api.reclutamientoqa.triple.com.do"
         
         elif modulo == "empleados":
-            api = "https://api.empleados.triple.com.do"
+            api = "https://api.empleadosqa.triple.com.do"
         elif modulo == "administracion":
-            api = "https://api.administracion.triple.com.do"
+            api = "https://api.administracionqa.triple.com.do"
         elif modulo == "framework":
-            api = "https://api.framework.triple.com.do"
+            api = "https://api.frameworkqa.triple.com.do"
         
           
         url = f"{api}/{endpoint}"
